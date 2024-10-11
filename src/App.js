575b221d918adbd9817657e6c828df243f0b37ae
Matheus Oliveira da Silva <oliveiradsmatheus@gmail.com>
2024-10-11 15:40:46 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>OK</p>
+        <p>TESTE</p>
+        <p>TESTE</p>
+        <p>TESTE</p>
+        <p>TESTE</p>
+        <p>TESTE</p>
       </header>
+      
+      <p>TESTE</p>
     </div>
   );
 }
