77b4ef342cb228f617e45f0ac2e913a02679d406
Matheus Oliveira da Silva <oliveiradsmatheus@gmail.com>
2024-10-11 15:55:27 -0300

    Instalação do React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" />
+        </Routes>
+      </BrowserRouter>
       <header className="App-header">
         <p>OK</p>
         <h2>OK</h2>
         <h2>OK</h2>
         <h2>OK</h2>
       </header>
-      
+
       <p>TESTE</p>
     </div>
   );
