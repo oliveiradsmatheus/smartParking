86802faaf6084673ee5dfca086817f0fd3fb85f6
Luiz Gustavo Renner Scatalon <luizgustavo.zrs@hotmail.com>
2024-11-05 20:48:50 -0300

    att .ino

--- IDE/ESP32/ESP32.TEST.IO/ESP32.TEST.IO.ino
+++ IDE/ESP32/ESP32.TEST.IO/ESP32.TEST.IO.ino
@@ -19,12 +19,13 @@ bool WifiOn();
 void conectar();
 
 //==================== SETUP ====================//
-void setup() {
-    Serial.begin(115200);
-    Serial1.begin(115200, SERIAL_8N1, 16, 17); // Serial para comunicação com o Arduino Mega
-    pinMode(2, OUTPUT);
+void setup()
+{
+	Serial.begin(115200);
+	Serial1.begin(115200, SERIAL_8N1, 16, 17); // Serial para comunicação com o Arduino Mega
+	pinMode(2, OUTPUT);
 
-    // Serial.println("Manda o timeout em 5s !");
+	// Serial.println("Manda o timeout em 5s !");
 	// delay(5000);
 	// if(Serial.available()){
 	// 	String stringTimeout = Serial.readStringUntil('\n');
@@ -34,53 +35,56 @@ void setup() {
 	// 		Serial.println(timeout);
 	// 	}
 	// }
-    
-    // Conectar ao WiFi
-    conectar();
 
-    // Configurar o Socket.IO
-    socketIO.begin(IP, PORTA);
-    socketIO.on("event", lidarEvento); // Configurar o evento de callback
+	// Conectar ao WiFi
+	conectar();
+
+	// Configurar o Socket.IO
+	socketIO.begin(IP, PORTA);
+	socketIO.on("event", lidarEvento); // Configurar o evento de callback
 }
 
-void loop() {
-    socketIO.loop(); // Atualizar estado do Socket.IO
+void loop()
+{
+	socketIO.loop(); // Atualizar estado do Socket.IO
 
-    // Verificar se há uma requisição do Mega na Serial1 e processá-la
-    if (Serial1.available() && Serial1.readString() == "on") {
-        if (WifiOn()) {
-            Serial1.write("on"); // Wifi ON
-            String resp = EsperarRespostaMega();
-            if (resp != "") {
-                ProcessarRequisicaoHTTP(resp); // Executa a requisição HTTP com base na resposta
-            }
-        }
-        else {
-            Serial1.print("off"); // Wifi OFF
-            Serial.println("Reconectando ao WiFi...");
-            conectar();
-        }
-    }
-    
-    // Reconectar ao WiFi se desconectado
-    if (!WifiOn()) {
-        Serial.println("Reconectando ao WiFi...");
-        conectar();
-    }
+	// Verificar se há uma requisição do Mega na Serial1 e processá-la
+	if (Serial1.available() && Serial1.readString() == "on") {
+		if (WifiOn()) {
+			Serial1.write("on"); // Wifi ON
+			String resp = EsperarRespostaMega();
+			if (resp != "") {
+				ProcessarRequisicaoHTTP(resp); // Executa a requisição HTTP com base na resposta
+			}
+		}
+		else {
+			Serial1.print("off"); // Wifi OFF
+			Serial.println("Reconectando ao WiFi...");
+			conectar();
+		}
+	}
+
+	// Reconectar ao WiFi se desconectado
+	if (!WifiOn()) {
+		Serial.println("Reconectando ao WiFi...");
+		conectar();
+	}
 }
 
 // Função de evento para o Socket.IO
-void lidarEvento(const char* payload, size_t length) {
-    String message = String((char*)payload).substring(0, length);
-    Serial.print("Mensagem do banco de dados: ");
-    Serial.println(message);
+void lidarEvento(const char* payload, size_t length)
+{
+	String message = String((char*)payload).substring(0, length);
+	Serial.print("Mensagem do banco de dados: ");
+	Serial.println(message);
 
-    // Enviar atualização para o Mega via Serial1
-    Serial1.print(message);
+	// Enviar atualização para o Mega via Serial1
+	Serial1.print(message);
 }
 
-void ProcessarResposta(int httpResposta, HTTPClient &http) {
-    String resp = "", resp2 = "";
+void ProcessarResposta(int httpResposta, HTTPClient &http)
+{
+	String resp = "", resp2 = "";
 	if (httpResposta > 0) {
 		resp = http.getString();
 		Serial.print("RESP Servidor: " + resp);
@@ -100,80 +104,83 @@ void ProcessarResposta(int httpResposta, HTTPClient &http) {
 }
 
 // Função para ler resposta do Mega pela Serial1
-String EsperarRespostaMega() {
-    String resposta = "";
-    unsigned long startTime = millis();
-    Serial.println("\nAguardando resposta Mega...");
+String EsperarRespostaMega()
+{
+	String resposta = "";
+	unsigned long startTime = millis();
+	Serial.println("\nAguardando resposta Mega...");
 
-    while (millis() - startTime < timeout)
-    {
-        if (Serial1.available()) {
-            resposta = Serial1.readString();
-            Serial.println("Resposta recebida Mega: " + resposta);
-            break;
-        }
-    }
-    if (resposta == "")
-        Serial.println("Sem Resposta Mega!!!");
-    return resposta;
+	while (millis() - startTime < timeout) {
+		if (Serial1.available()) {
+			resposta = Serial1.readString();
+			Serial.println("Resposta recebida Mega: " + resposta);
+			break;
+		}
+	}
+	if (resposta == "")
+		Serial.println("Sem Resposta Mega!!!");
+	return resposta;
 }
 
 // Função para processar requisições HTTP baseadas nas mensagens do Mega
-void ProcessarRequisicaoHTTP(String resp) {
-    HTTPClient http;
-    int httpResposta;
-    String id, estado;
+void ProcessarRequisicaoHTTP(String resp)
+{
+	HTTPClient http;
+	int httpResposta;
+	String id, estado;
 
-    if (resp[0] == '0') {   // PATCH para tabela sensor PATCH:ID_SENSOR:ESTADO
-        id = resp.substring(2, resp.indexOf(':', 2));       // Extrai id
-        estado = resp.substring(resp.indexOf(':', 2) + 1);  // Extrai estado
-        Serial.print(id);
-        Serial.print(" / ");
-        Serial.println(estado);
-        http.begin("http://" + String(IP) + ":5000/sensores/" + id + "/" + estado);
-        httpResposta = http.PUT("");
-        ProcessarResposta(httpResposta, http);
-    }
-    else if (resp[0] == '1') {  // PATCH para tabela ocupacao PATCH:ID_Ocupacao
-        id = resp.substring(resp.indexOf(':') + 1);
-        Serial.println(id);
-        http.begin("http://" + String(IP) + ":5000/ocupacoes/" + id);
-        httpResposta = http.PUT("");
-        ProcessarResposta(httpResposta, http);
-    }
-    else if (resp[0] == '2') {  // POST para tabela ocupacao POST:ID_SENSOR
-        id = resp.substring(resp.indexOf(':') + 1);
-        Serial.println(id);
-        http.begin("http://" + String(IP) + ":5000/ocupacoes/" + id);
-        httpResposta = http.POST("");
-        ProcessarResposta(httpResposta, http);
-    }
-    else {
-        Serial.println("Mensagem Vazia !!!");
-    }
-    http.end();
+	if (resp[0] == '0') {   // PATCH para tabela sensor PATCH:ID_SENSOR:ESTADO
+		id = resp.substring(2, resp.indexOf(':', 2));       // Extrai id
+		estado = resp.substring(resp.indexOf(':', 2) + 1);  // Extrai estado
+		Serial.print(id);
+		Serial.print(" / ");
+		Serial.println(estado);
+		http.begin("http://" + String(IP) + ":5000/sensores/" + id + "/" + estado);
+		httpResposta = http.PUT("");
+		ProcessarResposta(httpResposta, http);
+	}
+	else if (resp[0] == '1') {  // PATCH para tabela ocupacao PATCH:ID_Ocupacao
+		id = resp.substring(resp.indexOf(':') + 1);
+		Serial.println(id);
+		http.begin("http://" + String(IP) + ":5000/ocupacoes/" + id);
+		httpResposta = http.PUT("");
+		ProcessarResposta(httpResposta, http);
+	}
+	else if (resp[0] == '2') {  // POST para tabela ocupacao POST:ID_SENSOR
+		id = resp.substring(resp.indexOf(':') + 1);
+		Serial.println(id);
+		http.begin("http://" + String(IP) + ":5000/ocupacoes/" + id);
+		httpResposta = http.POST("");
+		ProcessarResposta(httpResposta, http);
+	}
+	else {
+		Serial.println("Mensagem Vazia !!!");
+	}
+	http.end();
 }
 
-bool WifiOn() {
-    return WiFi.status() == WL_CONNECTED;
+bool WifiOn()
+{
+	return WiFi.status() == WL_CONNECTED;
 }
 
-void conectar() {
-    WiFi.mode(WIFI_OFF);
-    delay(1000);
-    int t = 10;
-    WiFi.mode(WIFI_STA);
-    delay(500);
-    WiFi.begin(REDE, SENHA);
-    while (WiFi.status() != WL_CONNECTED && t > 0) {
-        delay(1000);
-        Serial.print(".");
-        t--;
-    }
-    if (WiFi.status() == WL_CONNECTED) {
-        Serial.println("\nConectado...yeey :) !!!");
-    }
-    else {
-        Serial.println("\nFalha ao conectar ao WiFi !!!");
-    }
+void conectar()
+{
+	WiFi.mode(WIFI_OFF);
+	delay(1000);
+	int t = 10;
+	WiFi.mode(WIFI_STA);
+	delay(500);
+	WiFi.begin(REDE, SENHA);
+	while (WiFi.status() != WL_CONNECTED && t > 0) {
+		delay(1000);
+		Serial.print(".");
+		t--;
+	}
+	if (WiFi.status() == WL_CONNECTED) {
+		Serial.println("\nConectado...yeey :) !!!");
+	}
+	else {
+		Serial.println("\nFalha ao conectar ao WiFi !!!");
+	}
 }
--- IDE/ESP32/ESP32.ino
+++ IDE/ESP32/ESP32.ino
@@ -9,7 +9,7 @@ const char *SENHA = "desconhecido123";
 bool WifiOn();
 void conectar();
 String EsperarRespostaMEGA();
-void ProcessarResposta(int httpResposta, HTTPClient &http);
+void ProcessarResposta_API(int httpResposta, HTTPClient &http);
 
 //==================== SETUP ====================//
 void setup()
@@ -17,76 +17,52 @@ void setup()
 	Serial.begin(115200);
 	Serial1.begin(115200, SERIAL_8N1, 16, 17);
 	pinMode(2, OUTPUT);
-	// Serial.println("Manda o timeout em 5s !");
-	// delay(5000);
-	// if(Serial.available()){
-	// 	String stringTimeout = Serial.readStringUntil('\n');
-	// 	if(stringTimeout != ""){
-	// 		timeout = strtoul(stringTimeout.c_str(), NULL, 10);
-	// 		Serial.print("Timeout recebido: ");
-	// 		Serial.println(timeout);
-	// 	}
-	// }
 	conectar();
 }
 
 void loop()
 {
-	if (Serial1.available() && Serial1.readString() == "on")
-	{
-		if (WifiOn())
-		{
-			Serial1.write("on"); // Wifi ON
-			String resp = "";
-			resp = EsperarRespostaMEGA();
-			if (resp != "")
-			{
+	if (Serial1.available()) {
+		if (WifiOn()) {
+			String resp = Serial1.readString();
+			Serial.println("Resposta Recebida Mega: " + resp);
+			if (resp != "") {
 				int httpResposta;
 				HTTPClient http;
 				String payload = "", estado = "", id = "";
-				if (resp[0] == '0')
-				{													   // PATCH para tabela sensor PATCH:ID_SENSOR:ESTADO
-					id = resp.substring(2, resp.indexOf(':', 2));	   // Extrai id
-					estado = resp.substring(resp.indexOf(':', 2) + 1); // Extrai estado
-					Serial.print(id);
-					Serial.print(" / ");
-					Serial.println(estado);
+				if (resp[0] == '0') {	// PATCH para tabela sensor PATCH:ID_SENSOR:ESTADO
+					id = resp.substring(2, resp.indexOf(':', 2));	   		// Extrai id
+					estado = resp.substring(resp.indexOf(':', 2) + 1); 		// Extrai estado
 					http.begin("http://" + IP + ":5000/sensores/" + id + "/" + estado);
 					httpResposta = http.PUT("");
-					ProcessarResposta(httpResposta, http);
+					ProcessarResposta_API(httpResposta, http);
 				}
-				else if (resp[0] == '1')
-				{ // PATCH para tabela ocupacao PATCH:ID_Ocupacao
+				else if (resp[0] == '1') {		// PATCH para tabela ocupacao PATCH:ID_Ocupacao
 					id = resp.substring(resp.indexOf(':') + 1);
-					Serial.println(id);
 					http.begin("http://" + IP + ":5000/ocupacoes/" + id);
 					httpResposta = http.PUT("");
-					ProcessarResposta(httpResposta, http);
+					ProcessarResposta_API(httpResposta, http);
 				}
-				else if (resp[0] == '2')
-				{ // POST para tabela ocupacao POST:ID_SENSOR
+				else if (resp[0] == '2') {		// POST para tabela ocupacao POST:ID_SENSOR
 					id = resp.substring(resp.indexOf(':') + 1);
-					Serial.println(id);
 					http.begin("http://" + IP + ":5000/ocupacoes/" + id);
 					httpResposta = http.POST("");
-					ProcessarResposta(httpResposta, http);
+					ProcessarResposta_API(httpResposta, http);
 				}
-				else
-				{
-					Serial.println("Mensagem Vazia !!!");
+				else {
+					Serial.println("Msg Mega Vazia !!!");
 				}
 				http.end();
 			}
 		}
-		else
-		{
-			Serial1.print("off"); // Wifi OFF
+		else {
+			Serial1.print("WiFi Off");
 			Serial.println("Reconectando ao WiFi...");
 			conectar();
 		}
 	}
-	else if (!WifiOn())
-	{
+	
+	if (!WifiOn()) {
 		Serial.println("Reconectando ao WiFi...");
 		conectar();
 	}
@@ -102,10 +78,8 @@ String EsperarRespostaMEGA()
 	unsigned long startTime = millis();
 	Serial.println("\nAguardando resposta MEGA...");
 
-	while (millis() - startTime < timeout)
-	{
-		if (Serial1.available())
-		{
+	while (millis() - startTime < timeout) {
+		if (Serial1.available()) {
 			resposta = Serial1.readString();
 			Serial.println("Resposta recebida MEGA: " + resposta);
 			break;
@@ -115,36 +89,30 @@ String EsperarRespostaMEGA()
 		Serial.println("Sem Resposta MEGA!!!");
 	return resposta;
 }
-
-void ProcessarResposta(int httpResposta, HTTPClient &http)
+void ProcessarResposta_API(int httpResposta, HTTPClient &http)
 {
 	String resp = "", resp2 = "";
-	if (httpResposta > 0)
-	{
+	if (httpResposta > 0) {
 		resp = http.getString();
-		Serial.print("RESP Servidor: " + resp);
+		Serial.print("Resposta API: " + resp);
 		Serial1.print(resp);
 	}
-	else if (httpResposta == -1)
-	{
+	else if (httpResposta == -1) {
 		resp = String(httpResposta);
-		Serial.print("Servidor Offline!, ERRO: " + resp);
+		Serial.print("API Offline!, ERRO: " + resp);
 		Serial1.print(resp);
 	}
-	else
-	{
+	else {
 		resp = String(httpResposta);
 		resp2 = String(http.getString());
 		Serial.print("ERRO!: " + resp + " : " + resp2);
 		Serial1.print(resp2);
 	}
 }
-
 bool WifiOn()
 {
 	return WiFi.status() == WL_CONNECTED;
 }
-
 void conectar()
 {
 	WiFi.mode(WIFI_OFF);
@@ -153,18 +121,74 @@ void conectar()
 	WiFi.mode(WIFI_STA);
 	delay(500);
 	WiFi.begin(REDE, SENHA);
-	while (WiFi.status() != WL_CONNECTED && t > 0)
-	{
+	while (WiFi.status() != WL_CONNECTED && t > 0) {
 		delay(1000);
 		Serial.print(".");
 		t--;
 	}
-	if (WiFi.status() == WL_CONNECTED)
-	{
+	if (WiFi.status() == WL_CONNECTED) {
+		digitalWrite(2, HIGH);
 		Serial.println("\nConectado...yeey :)");
 	}
-	else
-	{
+	else {
 		Serial.println("\nFalha ao conectar ao WiFi");
 	}
 }
+
+//loop ORIGINAL
+/*void loop()
+{
+	if (Serial1.available() && Serial1.readString() == "on") {
+		if (WifiOn()) {
+			Serial1.write("on"); // Wifi ON
+			String resp = "";
+			resp = EsperarRespostaMEGA();
+			if (resp != "") {
+				int httpResposta;
+				HTTPClient http;
+				String payload = "", estado = "", id = "";
+				if (resp[0] == '0') {
+					// PATCH para tabela sensor PATCH:ID_SENSOR:ESTADO
+					id = resp.substring(2, resp.indexOf(':', 2));	   // Extrai id
+					estado = resp.substring(resp.indexOf(':', 2) + 1); // Extrai estado
+					Serial.print(id);
+					Serial.print(" / ");
+					Serial.println(estado);
+					http.begin("http://" + IP + ":5000/sensores/" + id + "/" + estado);
+					httpResposta = http.PUT("");
+					ProcessarResposta_API(httpResposta, http);
+				}
+				else if (resp[0] == '1') {
+					// PATCH para tabela ocupacao PATCH:ID_Ocupacao
+					id = resp.substring(resp.indexOf(':') + 1);
+					Serial.println(id);
+					http.begin("http://" + IP + ":5000/ocupacoes/" + id);
+					httpResposta = http.PUT("");
+					ProcessarResposta_API(httpResposta, http);
+				}
+				else if (resp[0] == '2') {
+					// POST para tabela ocupacao POST:ID_SENSOR
+					id = resp.substring(resp.indexOf(':') + 1);
+					Serial.println(id);
+					http.begin("http://" + IP + ":5000/ocupacoes/" + id);
+					httpResposta = http.POST("");
+					ProcessarResposta_API(httpResposta, http);
+				}
+				else {
+					Serial.println("Mensagem Vazia !!!");
+				}
+				http.end();
+			}
+		}
+		else {
+			Serial1.print("off"); // Wifi OFF
+			Serial.println("Reconectando ao WiFi...");
+			conectar();
+		}
+	}
+	
+	if (!WifiOn()) {
+		Serial.println("Reconectando ao WiFi...");
+		conectar();
+	}
+}*/
