6a453de68dfc6502058bcda98b09356a8022955c
Luiz Gustavo Renner Scatalon <luizgustavo.zrs@hotmail.com>
2024-11-08 17:47:07 -0300

    adicionando codigo dos Arduinos, troca pra .ino se quiser

---
+++ IDE/ESP32.cpp
@@ -0,0 +1,144 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+String IP = "192.168.236.229";
+unsigned long timeout = 5000;
+const char *REDE = "N5";
+const char *SENHA = "desconhecido123";
+
+bool WifiOn();
+void conectar();
+void EsperarRespostaIP();
+void ProcessarResposta_API(int httpResposta, HTTPClient &http);
+
+//==================== SETUP ====================//
+// cppcheck-suppress unusedFunction
+void setup() {
+	Serial.begin(115200);
+	Serial1.begin(115200, SERIAL_8N1, 16, 17);
+	pinMode(2, OUTPUT);
+	EsperarRespostaIP();
+	conectar();
+}
+
+// cppcheck-suppress unusedFunction
+void loop() {
+	if (Serial1.available()) {
+		if (WifiOn()) {
+			String resp = Serial1.readString();
+			Serial.println("\n\n\nResposta Recebida Mega: " + resp);
+			if (resp != "") {
+				int httpResposta;
+				HTTPClient http;
+				String payload = "", estado = "", id = "";
+				if (resp[0] == '0') {	// PATCH para tabela sensor PATCH:ID_SENSOR:ESTADO
+					id = resp.substring(2, resp.indexOf(':', 2));	   		// Extrai id
+					estado = resp.substring(resp.indexOf(':', 2) + 1); 		// Extrai estado
+					Serial.println("Msg Mega: "+id+" / "+estado);
+					http.begin("http://" + IP + ":5000/sensores/" + id + "/" + estado);
+					httpResposta = http.PUT("");
+					ProcessarResposta_API(httpResposta, http);
+				}
+				else if (resp[0] == '1') {		// PATCH para tabela ocupacao PATCH:ID_Ocupacao
+					id = resp.substring(resp.indexOf(':') + 1);
+					Serial.println("Msg Mega: "+id);
+					http.begin("http://" + IP + ":5000/ocupacoes/" + id);
+					httpResposta = http.PUT("");
+					ProcessarResposta_API(httpResposta, http);
+				}
+				else if (resp[0] == '2') {		// POST para tabela ocupacao POST:ID_SENSOR
+					id = resp.substring(resp.indexOf(':') + 1);
+					Serial.println("Msg Mega: "+id);
+					http.begin("http://" + IP + ":5000/ocupacoes/" + id);
+					httpResposta = http.POST("");
+					ProcessarResposta_API(httpResposta, http);
+				}
+				else {
+					Serial.println("Msg Mega Vazia !!!");
+				}
+				http.end();
+			}
+		}
+		else {
+			Serial1.print("WiFi Off");
+			Serial.println("Reconectando ao WiFi...");
+			conectar();
+		}
+	}
+	
+	if (!WifiOn()) {
+		Serial.println("Reconectando ao WiFi...");
+		conectar();
+	}
+}
+//===============================================//
+
+//-----------------------------------------------//
+
+//===============================================//
+void ProcessarResposta_API(int httpResposta, HTTPClient &http) {
+	String resp = "", resp2 = "";
+	if (httpResposta > 0) {
+		resp = http.getString();
+		Serial.print("Resposta API: " + resp);
+		Serial1.print(resp);
+	}
+	else if (httpResposta == -1) {
+		resp = String(httpResposta);
+		Serial.print("API Offline!, ERRO: " + resp);
+		Serial1.print(resp);
+	}
+	else {
+		resp = String(httpResposta);
+		resp2 = String(http.getString());
+		Serial.print("ERRO!: " + resp + " : " + resp2);
+		Serial1.print(resp2);
+	}
+}
+
+void EsperarRespostaIP() {
+	String resposta = "";
+	unsigned long startTime = millis();
+	Serial.println("Digite o IP da req p/ HTTP !");
+	while (millis() - startTime < timeout) {
+		if (Serial.available()) {
+			resposta = Serial.readStringUntil('\n');
+			Serial.println("Novo IP p/ req HTTP: " + resposta);
+			IP = resposta;
+			break;
+		}
+	}
+	if (resposta == ""){
+		Serial.println("Sem Resposta do Novo IP p/ req HTTP !!!");
+		Serial.println("Alocado: IP Padrao " + IP);
+	}
+	else{
+		Serial.println("Alocado: Novo IP " + IP);
+	}
+}
+
+bool WifiOn() {
+	return WiFi.status() == WL_CONNECTED;
+}
+
+void conectar() {
+	WiFi.mode(WIFI_OFF);
+	delay(1000);
+	int t = 10;
+	WiFi.mode(WIFI_STA);
+	delay(500);
+	WiFi.begin(REDE, SENHA);
+	while (WiFi.status() != WL_CONNECTED && t > 0) {
+		delay(1000);
+		Serial.print(".");
+		t--;
+	}
+	if (WiFi.status() == WL_CONNECTED) {
+		digitalWrite(2, HIGH);
+		Serial.println("\nConectado...yeey :)");
+	}
+	else {
+		Serial.println("\nFalha ao conectar ao WiFi");
+	}
+}
---
+++ IDE/MEGA2560.cpp
@@ -0,0 +1,235 @@
+// Baixar no gerenciador de bibliotecas 'NewPing by Tim Eckel'
+#include <NewPing.h>
+#include <Arduino.h>
+
+#define TRIG_1 24 // OK
+#define ECHO_1 26 // OK
+
+#define TRIG_2 28 // OK
+#define ECHO_2 30 // OK
+
+#define TRIG_3 32 // OK
+#define ECHO_3 34 // OK
+
+#define LED_1G 47 // OK
+#define LED_1Y 45 // OK
+#define LED_1R 43 // OK
+
+#define LED_2G 41 // OK
+#define LED_2Y 39 // OK
+#define LED_2R 37 // OK
+
+#define LED_3G 35 // OK
+#define LED_3Y 33 // OK
+#define LED_3R 31 // OK
+
+#define MAX_DISTANCIA 150 // em cm
+
+NewPing sonar1 (TRIG_1, ECHO_1, MAX_DISTANCIA);
+NewPing sonar2 (TRIG_2, ECHO_2, MAX_DISTANCIA);
+NewPing sonar3 (TRIG_3, ECHO_3, MAX_DISTANCIA);
+
+int Dist_MAX = 30, Dist_MIN = 5, TempoAnalise = 6; // cm , seg
+unsigned long timeout = 5000;
+
+char Estado[3] = {'D', 'D', 'D'};
+int idSensor[3] = {1, 2, 3}, idOcupacao[3] = {0, 0, 0}, TempoVaga[3] = {0, 0, 0};
+
+void SetLed(int LED_R, int LED_Y, int LED_G, int TempoVaga[], int Distancia, const int idSensor[], int index);
+long MedirDistancia(int TRIG, int ECHO);
+void ExibirDistancia(long Distancia, int num);
+
+void setup() {
+	Serial.begin(115200);
+	Serial1.begin(115200); // Serial de comunicao do ESP32, n precisa definir o RX1 e TX1 pq ja é padrao o nome Serial1 pra ele;
+
+	// Configurar pinos como saída e entrada
+	pinMode(TRIG_1, OUTPUT);
+	pinMode(ECHO_1, INPUT);
+	pinMode(TRIG_2, OUTPUT);
+	pinMode(ECHO_2, INPUT);
+	pinMode(TRIG_3, OUTPUT);
+	pinMode(ECHO_3, INPUT);
+
+	// Configurar LED's de saída
+	pinMode(LED_1R, OUTPUT);
+	pinMode(LED_1Y, OUTPUT);
+	pinMode(LED_1G, OUTPUT);
+	pinMode(LED_2R, OUTPUT);
+	pinMode(LED_2Y, OUTPUT);
+	pinMode(LED_2G, OUTPUT);
+	pinMode(LED_3R, OUTPUT);
+	pinMode(LED_3Y, OUTPUT);
+	pinMode(LED_3G, OUTPUT);
+
+	digitalWrite(LED_1G, HIGH);
+	digitalWrite(LED_2G, HIGH);
+	digitalWrite(LED_3G, HIGH);
+
+	delay(1000);
+}
+
+void loop() {
+	int Distancia1 = sonar1.ping_cm(); 
+	int Distancia2 = sonar2.ping_cm();
+	int Distancia3 = sonar3.ping_cm();
+   
+	ExibirDistancia(Distancia1, 1);
+	ExibirDistancia(Distancia2, 2);
+	ExibirDistancia(Distancia3, 3);
+	Serial.println("");
+
+	SetLed(LED_1R, LED_1Y, LED_1G, TempoVaga, Distancia1, idSensor, 0);
+	SetLed(LED_2R, LED_2Y, LED_2G, TempoVaga, Distancia2, idSensor, 1);
+	SetLed(LED_3R, LED_3Y, LED_3G, TempoVaga, Distancia3, idSensor, 2);
+
+	delay(1000);
+}
+
+//=============== Funcao Principal ===================//
+void Processar_ESTADO(String msg, int index, char novoEstado);
+void Processar_POST(String msg, int index);
+void Processar_PUT(String msg, int index);
+String EsperarRespostaESP_API();
+void SetLed(int LED_R, int LED_Y, int LED_G, int TempoVaga[], int Distancia, const int idSensor[], int index) {
+	String MSG_ENVIAR = "", RESPOSTA = "";
+	if (TempoVaga[index] <= 0 && (Distancia > Dist_MAX || Distancia < Dist_MIN)) {	  // Vaga Disponivel
+		digitalWrite(LED_R, LOW);
+		digitalWrite(LED_Y, LOW);
+		digitalWrite(LED_G, HIGH);
+		if (Estado[index] == 'O') {						// req UPDATE para a tabela de sensores ESTADO 'Disponivel'
+			MSG_ENVIAR = "0:" + String(idSensor[index]) + ":D";		// Formato: PATCH:ID_SENSOR:ESTADO
+			Serial1.print(MSG_ENVIAR);								// manda Mensagem para o ESP32
+			Serial.println("\nMsg Enviada: " + MSG_ENVIAR);
+			RESPOSTA = EsperarRespostaESP_API();
+			Processar_ESTADO(RESPOSTA, index, 'D');
+
+			MSG_ENVIAR = "1:" + String(idOcupacao[index]); 		// Formato: PATCH:ID_Ocupacao
+			Serial1.print(MSG_ENVIAR);
+			Serial.println("\nMsg Enviada: " + MSG_ENVIAR);
+			RESPOSTA = EsperarRespostaESP_API();
+			Processar_PUT(RESPOSTA, index); 					// dentro da func ele ja zera o valor do idOcupacao[index]
+		}
+		else if (Estado[index] == 'A') {				// req UPDATE para a tabela de sensores ESTADO 'Disponivel'
+			MSG_ENVIAR = "0:" + String(idSensor[index]) + ":D"; 	// Formato: PATCH:ID_SENSOR:ESTADO
+			Serial1.print(MSG_ENVIAR);
+			Serial.println("\nMsg Enviada: " + MSG_ENVIAR);;
+			RESPOSTA = EsperarRespostaESP_API();
+			Processar_ESTADO(RESPOSTA, index, 'D');
+		}
+	}
+	else if (TempoVaga[index] < TempoAnalise && Distancia < Dist_MAX && Distancia > Dist_MIN) {	// Vaga em Analise
+		if (TempoVaga[index] > 2 && Estado[index] != 'O') {												// Vaga esta para Analise  e pode sinalizar para o ESP32
+			digitalWrite(LED_R, LOW);
+			digitalWrite(LED_Y, HIGH);
+			digitalWrite(LED_G, LOW);
+			if (Estado[index] == 'D') {							// req UPDATE para a tabela de sensores ESTADO 'Analise'
+				MSG_ENVIAR = "0:" + String(idSensor[index]) + ":A"; 		// Formato: PATCH:ID_SENSOR:ESTADO
+				Serial1.print(MSG_ENVIAR);
+				Serial.println("\nMsg Enviada: " + MSG_ENVIAR);
+				RESPOSTA = EsperarRespostaESP_API();
+				Processar_ESTADO(RESPOSTA, index, 'A');
+			}
+		}
+		if (TempoVaga[index] <= TempoAnalise) {
+			TempoVaga[index]++;
+		}
+	}
+	else if (Distancia < Dist_MAX && Distancia > Dist_MIN) {	// Vaga Ocupada
+		if (TempoVaga[index] > (TempoAnalise + 2)) {							// Vaga esta para Ocupada e pode sinalizar para o ESP32
+			digitalWrite(LED_R, HIGH);
+			digitalWrite(LED_Y, LOW);
+			digitalWrite(LED_G, LOW);
+			if (Estado[index] == 'A') {									// req UPDATE para a tabela de sensores ESTADO 'Ocupado'
+				MSG_ENVIAR = "0:" + String(idSensor[index]) + ":O"; 	// Formato: PATCH:ID_SENSOR:ESTADO
+				Serial1.print(MSG_ENVIAR);								// Enviar mensagem para o ESP32
+				Serial.println("\nMsg Enviada: " + MSG_ENVIAR);
+				RESPOSTA = EsperarRespostaESP_API();
+				Processar_ESTADO(RESPOSTA, index, 'O');
+				if (idOcupacao[index] == 0) {							// mandar idSensor[index], req POST para a tabela de ocupaçoes & idOcupacao[index] = resposta API
+					MSG_ENVIAR = "2:" + String(idSensor[index]); 		// Formato: POST:ID_SENSOR
+					Serial.println(MSG_ENVIAR);
+					Serial1.print(MSG_ENVIAR);
+					Serial.println("\nMsg Enviada: " + MSG_ENVIAR);
+					RESPOSTA = EsperarRespostaESP_API();
+					Processar_POST(RESPOSTA, index);
+				}
+			}
+		}
+		else {
+			TempoVaga[index]++;
+		}
+	}
+	else {
+		if (Estado[index] == 'O') {		// TempoVaga[index] chegar em 0 então pode sinalizar para o ESP32
+			TempoVaga[index] -= 2;
+		}
+		else {
+			TempoVaga[index] = 0; // passou de analise para disponivel
+		}
+	}
+}
+//====================================================//
+
+
+//=============== Esperar Respostas ==================//
+String EsperarRespostaESP_API() {
+	String resposta = "";
+	unsigned long startTime = millis();
+	Serial.println("Aguardando resposta API...");
+
+	while (millis() - startTime < timeout) {
+		if (Serial1.available()) {
+			resposta = Serial1.readString();
+			Serial.println("Resposta recebida API: " + resposta);
+			break;
+		}
+	}
+	if (resposta == "")
+		Serial.println("Sem Resposta da API !!!");
+	return resposta;
+}
+//====================================================//
+
+
+//=============== Processar Respostas ================//
+void Processar_ESTADO(String msg, int index, char novoEstado) {
+	if (msg != "" && msg == "Estado Atualizado") {
+		Estado[index] = novoEstado;
+		Serial.println("\nTRUE: req Update Estado Sensor: " + String(Estado[index]));
+	}
+	else {
+		Serial.println("\nFALSE: req Update Estado Sensor: " + msg);
+	}
+}
+void Processar_POST(String msg, int index) {
+	if (msg != "" && msg[0] == 'O' && msg[1] == 'K') {
+		String aux = msg.substring(msg.indexOf(':') + 1);
+		int id = atoi(aux.c_str());
+		idOcupacao[index] = id;
+		Serial.println("\nTRUE: req POST Da Ocupacao | ID_Ocupacao: " + String(idOcupacao[index]));
+	}
+	else {
+		Serial.println("\nFALSE: req POST Da Ocupacao: " + msg);
+	}
+}
+void Processar_PUT(String msg, int index) {
+	if (msg != "" && msg == "Ocupacao Atualizada") {
+		Serial.println("\nTRUE: req PUT para Ocupacao");
+		idOcupacao[index] = 0;
+	}
+	else {
+		Serial.println("\nFALSE: req PUT para Ocupacao: " + msg);
+	}
+}
+//====================================================//
+
+
+//=============== Processar Distancias ===============//
+void ExibirDistancia(long Distancia, int num) {
+	Serial.print("Sensor [ ");
+	Serial.print(num);
+	Serial.print(" ]:  ");
+	Serial.print(Distancia);
+	Serial.println("cm");
+}
